79152ec7f63f778d0e1d8195edd7a780399e16fb
Jason Welch <jwelch@calrom.com>
2026-02-07 13:38:14 +0000

    Clean metadata

---
+++ make.bat
@@ -0,0 +1,7 @@
+python makemeta.py "2025.09 (Sep)"
+python makemeta.py "2025.10 (Oct)"
+python makemeta.py "2025.11 (Nov)"
+python makemeta.py "2025.12 (Dec)"
+python makemeta.py "2026.01 (Jan)"
+python makemeta.py "2026.02 (Feb)"
+sigal build --debug images/
